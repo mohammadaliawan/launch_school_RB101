49047420001a2da8b29c26a6f6c2908fc757ae9c
Mohammad Ali <aliawan87@gmail.com>
2019-07-17 12:51:24 +0500

    Final Modifications to calc_bonus and calc_messages.yml

--- lesson_2/calc_bonus_features/calc_bonus.rb
+++ lesson_2/calc_bonus_features/calc_bonus.rb
@@ -2,105 +2,132 @@ require 'yaml'
 MESSAGES = YAML.load_file('calc_messages.yml')
 
 loop do
-  Kernel.puts("For Espaniol type 'es', For English type 'en'")
-  LANGUAGE = Kernel.gets().chomp()
-  break if LANGUAGE == 'en' || LANGUAGE == 'es'
+    Kernel.puts(">> For Espaniol type 'es', For English type 'en'")
+    LANGUAGE = Kernel.gets().chomp()
+    break if LANGUAGE == 'en' || LANGUAGE == 'es'
+    Kernel.puts(">> Invalid answer! Please enter 'es' for spanish or 'en' for English")
+end
+
+def valid_name?(name)
+  unless (/\d/ =~ name) || (name.empty?) || (/\W/ =~ name)
+    return true
+  end
+end
+
+def valid_number?(number, operation)
+  case operation 
+  when nil, "1", "2", "3"
+    number.to_i.to_s == number || number.to_f.to_s ==  number
+  when "4"
+    (number.to_i.to_s == number || number.to_f.to_s ==  number) && (number.to_f != 0.0)
+  end
+end
+
+def valid_operator?(operator)
+  return true if %w(1 2 3 4).include?(operator)
+end
+
+def prompt(str)
+  Kernel.puts(">> #{str}")
 end
 
 def messages(message, lang = LANGUAGE)
   MESSAGES[lang][message]
 end
 
-def prompt(message)
-  Kernel.puts("=> #{message}")
-end
-
-def number?(number)
-  number.to_i.to_s == number || number.to_f.to_s == number
-end
-
-def operator_to_msg(op)
-  msg = case op
-        when '1'
-          'Adding'
-        when '2'
-          'Subtracting'
-        when '3'
-          'Multiplying'
-        when '4'
-          'Dividing'
-        end
-  msg
-end
-
-prompt(messages('welcome'))
-
-name = ''
-loop do # Input name loop
-  name = Kernel.gets().chomp()
-
-  if name.empty?
-    prompt(MESSAGES[LANGUAGE]['valid_name'])
-  else
-    break
-  end
-end
-
-prompt("Hello, #{name}")
-
-loop do #  main loop
-  number1 = '' # Input first number loop
+def get_number(num, operation = nil)
   loop do
-    prompt(messages('number1'))
-    number1 = Kernel.gets().chomp()
-    break if number?(number1)
-    prompt(messages('valid_number'))
+    prompt(messages('number1')) if num == 1
+    prompt(messages('number2')) if num == 2
+    number = gets().chomp()
+    return number if valid_number?(number, operation)
+    if num == 1 || (num == 2 && %w(1 2 3).include?(operation))
+      prompt(messages('invalid_number1')) 
+    else
+      prompt(messages('invalid_number2'))
+    end
   end
+end
 
-  number2 = '' # Input second number loop
-  loop do
-    prompt(messages('number2'))
-    number2 = Kernel.gets().chomp()
-    break if number?(number2)
-    prompt(messages('valid_number'))
-  end
-
-  operator_prompt = <<-MSG # operator prompt message
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
+def get_operation
+  operation_msg =<<~MSG
+  Please enter the operation you want to perform
+    1) Add
+    2) Subtract
+    3) Multiply
+    4) Divide
   MSG
-
-  prompt(operator_prompt)
-
-  operation = ''
-  loop do # operation input loop
-    operation = gets().chomp()
-    break if %w(1 2 3 4).include?(operation)
-    prompt(messages('valid_operator'))
+  prompt(operation_msg)
+  loop do
+    operator = Kernel.gets().chomp()
+    return operator if valid_operator?(operator)
+    prompt(messages('invalid_operator'))
   end
-
-  prompt("#{operator_to_msg(operation)} the two numbers...")
-
-  result = case operation
-           when '1'
-             number1.to_f + number2.to_f
-           when '2'
-             number1.to_f - number2.to_f
-           when '3'
-             number1.to_f * number2.to_f
-           when '4'
-             number1.to_f / number2.to_f
-           end
-
-  prompt("The result is #{result}")
-
-  prompt(messages('again'))
-  answer = gets().chomp()
-
-  break unless answer.downcase.start_with?("y")
 end
 
-prompt(messages('goodbye'))
+def find_result(num1, num2, operation)
+  result = case operation
+           when "1" then num1.to_f + num2.to_f
+           when "2" then num1.to_f - num2.to_f
+           when "3" then num1.to_f * num2.to_f
+           when "4" then num1.to_f / num2.to_f
+           end
+  result
+end
+
+def greet
+  prompt(messages('welcome'))
+  loop do
+    name = Kernel.gets().chomp()
+    return name if valid_name?(name)
+    prompt(messages('invalid_name'))
+  end
+end
+
+def operation_to_sym(operation)
+  sym = case operation
+        when "1" then "+"
+        when "2" then "-"
+        when "3" then "*"
+        when "4" then "/"
+        end
+  sym
+end
+
+def get_answer
+  prompt(messages('again'))
+  answer = Kernel.gets().chomp().downcase()
+  until %w(y yes n no).include?(answer)
+    prompt(messages('invalid_answer'))
+    answer = Kernel.gets().chomp()
+  end
+  answer
+end
+
+def display_result(number1, number2, operation, result)
+  prompt("#{number1} #{operation_to_sym(operation)} #{number2} = #{result}")
+end
+
+name = greet()
+Kernel.puts("Hello, #{name}")
+
+#Main Loop Starts Here
+loop do
+  number1 = get_number(1)
+
+  operation = get_operation()
+
+  number2 = get_number(2, operation)
+
+  result = find_result(number1, number2, operation)
+
+  display_result(number1, number2, operation, result)
+
+  again_answer = get_answer
+
+  break if %w(n no).include?(again_answer)
+  system('clear')
+end
+
+prompt(messages('goodbye') + " #{name}")
+
