403992928f173fb8ca5de85c31a40fadaab247a2
Mohammad Ali <aliawan87@gmail.com>
2021-07-13 10:36:52 +0500

    updated

--- small_problems_2/easy_1/repeat.rb
+++ small_problems_2/easy_1/repeat.rb
@@ -15,9 +15,103 @@ Hello
 
 # PEDAC
 
-# The Problem
+U:
+- A method that takes two arguments
+- one string, one integer
+- prints the string integer number of times
+- input: one string, one integer
+- output: the string printed integer number of times on seperate line4s
+Explicit:
+- write a method that takes
+- two arguments
+- the method should print the string integer number of times
+Implicit: (from the test case)
+- first string and second integer
+- string should be printed on seperate lines with no lines in between
 
-A method that
+Questions:
+- What should the method return? meaningful value?
+No meaningful value
+- What should the method do if only the string is passed?
+Default integer value is 0 and print error message: "Integer not provided!"
+- What should the method do if only the integer is passed?
+Default string value is error message : "String not provided" printed once only
+- What if the first argument is not a string?
+give error message: "First argument is not a string" printed once only
+- What if the second argument is not an integer?
+give error message: "Second argument is not an integer!"
+
+
+DS:
+inputs:
+- first string and second integer
+OR edge cases:
+- first not a string and second integer
+- firt string and second not an integer
+- no inputs--> Default values
+- only string provided
+- only integer provided
+
+Algo:
+
+- Given two arguments
+- Check if both arguments are passed
+If both arguments are passed
+  - convert the first to a string
+  - Validate the second argument is an integer
+  - If the second argument is a valid integer
+    - print the string integer number of times
+  - if second is not a valid integer
+    - print error msg: "second argument is not a valid integer"
+If integer is not passed
+- print error message "Integer not provided"
+if both not passed
+- print error message "No Inputs given. Required input: first string, second integer "
+
+Implementation details:
+
+- Check if both arguments are passed
+ - Use an if conditional to check if the first argument and second argument are the default values
+    - print " No Inputs..."
+  - else if string == default && integer != default
+   - print "String not given"
+  - else if stirng !- default && integer == default
+    - print "Integer not given"
+
+
 
 =end
 
+DEFAULT_STR = "String not provided"
+DEFAULT_INT = "Integer not provided"
+
+def both_arguments_given?(str, int)
+  if str == DEFAULT_STR && int == DEFAULT_INT
+    puts "No Inputs given. Required input: first string, second integer "
+  elsif str != DEFAULT_STR && int == DEFAULT_INT
+    puts DEFAULT_INT
+  else
+    true
+  end
+end
+
+def valid_int?(num)
+  num.to_i == num
+end
+
+def repeat(str = DEFAULT_STR, int = DEFAULT_INT)
+  if both_arguments_given?(str, int)
+    if valid_int?(int)
+      int.times do |_|
+        puts str
+      end
+    else
+      puts "Second argument is not a valid integer!"
+    end
+  end
+end
+
+repeat("Hello", 2)
+repeat("Hello", 2.5)
+repeat("Hello")
+repeat(1,2)
---
+++ written_assessment/strategy.md
@@ -0,0 +1,31 @@
+Topics to be prepared:
+
+Test strategy
+  • Set 3 -  45mins timer
+  • Learn select, each and map throughly
+Test strategy
+  • Time yourself
+  • Answer 7/8 questions per hour to know you will be ready
+  • Many questions will have sub questions
+  
+Topics to be strong on
+  • Variables as Pointers -
+    ○ https://launchschool.com/books/ruby/read/more_stuff#variables_as_pointers
+  • Variable Scope
+    ○ https://launchschool.com/lessons/a0f3cd44/assignments/9e9e907c
+  • Object Passing / Mutability of Objects
+    ○ https://launchschool.com/lessons/a0f3cd44/assignments/4b1ad598
+    ○ https://launchschool.medium.com/variable-references-and-mutability-of-ruby-objects-4046bd5b6717
+    ○ https://launchschool.medium.com/ruby-objects-mutating-and-non-mutating-methods-78023d849a5f
+    ○ https://launchschool.medium.com/object-passing-in-ruby-pass-by-reference-or-pass-by-value-6886e8cdc34a
+  • Return and Puts
+    ○ https://launchschool.com/books/ruby/read/methods#putsvsreturnthesequel
+  • Collection:
+    ○ Iteration
+    ○ Selection
+    ○ Transformation
+  • More methods
+    ○ https://launchschool.com/lessons/85376b6d/assignments/d86be6b5
+  • How Sorting works
+
+  
