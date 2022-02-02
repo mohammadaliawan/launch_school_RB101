12a20aad521fb1e1cf1ca8519214b4c01a02e0fb
Mohammad ALi <aliawan87@gmail.com>
2022-02-02 14:54:26 +0500

    small problems easy 8

---
+++ small_problems_2/easy_8/all_substrings.rb
@@ -0,0 +1,117 @@
+=begin
+
+Write a method that returns a list of all substrings of a string.
+The returned list should be ordered by where in the string the substring begins.
+
+This means that all substrings that start at position 0 should come first,
+then all substrings that start at position 1, and so on.
+
+Since multiple substrings will occur at each position,
+the substrings at a given position should be returned in order from shortest to longest.
+
+You may (and should) use the leading_substrings method you wrote in the previous exercise:
+
+Examples:
+
+Copy Code
+substrings('abcde') == [
+  'a', 'ab', 'abc', 'abcd', 'abcde',
+  'b', 'bc', 'bcd', 'bcde',
+  'c', 'cd', 'cde',
+  'd', 'de',
+  'e'
+]
+
+
+First I am going to read through the problem a few times and understand the test cases provided. Then I am going
+deconstruct the problem into its requirements and define the rules and boundaries of the problem.
+
+------ PED:- 0 MIN to 12 MIN --------------
+
+
+**Input** :stirng
+**Output** : array of all substrings
+
+**Explicit Requirements/Rules/Boundaries**
+- return all substrings of a string
+- the list should be ordered by where in the string the substring begins
+  - starting from substring which start at index 0 , then 1, and so on
+- the list should also be ordered with respect to the length of the substring in ascending order
+
+
+**Implicit Requirements/Rules/Boundaries**
+- return all the substirng in one array
+
+
+**Special Terms**
+-
+
+Now I am going to go through the test cases in more detail and map out the high level sequence of manual steps
+involved in going from the input to the output
+
+**Examples/Test Cases**
+- input: 'abcde'
+- Output:
+
+
+ [
+  'a', 'ab', 'abc', 'abcd', 'abcde',
+  'b', 'bc', 'bcd', 'bcde',
+  'c', 'cd', 'cde',
+  'd', 'de',
+  'e'
+]
+
+for starting index iterate from 0 to last char size - 1
+for end index iterate from current starting index to size - 1
+
+
+**Questions**
+-
+
+
+**Data Structures**
+- input: string
+- Output: array of substrings
+- Intermediate:
+- Storage:
+
+--------------- A:- 12 MIN to 20 MIN------------
+
+**Algorithm**
+- Given a string
+- init an array [] substrings
+- For starting_idnex from 0 to size - 1
+  - For end index from starting_index to siZe - 1
+    - get the current substring with starting_index,  end_indx
+- return the substrings
+
+
+**Implementation details**
+-
+
+
+-------------- C: 20 MIN to 25 MIN ------------------------
+
+**CodeWithIntent****
+=end
+
+
+def substrings(str)
+  subs = []
+
+  0.upto(str.size - 1) do |start_index|
+    start_index.upto(str.size - 1) do |end_index|
+      subs << str.slice(start_index..end_index)
+    end
+  end
+  subs
+end
+
+p substrings('abcde') #== [
+#   'a', 'ab', 'abc', 'abcd', 'abcde',
+#   'b', 'bc', 'bcd', 'bcde',
+#   'c', 'cd', 'cde',
+#   'd', 'de',
+#   'e'
+# ]
--- small_problems_2/easy_8/sum_of_sums.rb
+++ small_problems_2/easy_8/sum_of_sums.rb
@@ -17,15 +17,22 @@ deconstruct the problem into its requirements and define the rules and boundarie
 ------ PED:- 0 MIN to 12 MIN --------------
 
 
-**Input** :
-**Output** :
+**Input** : array of numbers
+**Output** : sum of the sum of leading subsequences
 
 **Explicit Requirements/Rules/Boundaries**
--
-
+- write a method
+- takes an array of nums as argument
+- returns the sum of the sums of each leading subsequence for that Array
+- subsequence : a sequence of consecutive elements of the array
+- leading: starting from the first element
+- sum of each leading subsequnce: sum of a sequence of consecutive elements of the array starting from the first one
+- sum of the above sums
+- array will contain atleast one number
 
 **Implicit Requirements/Rules/Boundaries**
--
+- if only one integer in the array return that integer
+
 
 
 **Special Terms**
@@ -33,9 +40,14 @@ deconstruct the problem into its requirements and define the rules and boundarie
 
 Now I am going to go through the test cases in more detail and map out the high level sequence of manual steps
 involved in going from the input to the output
+
 **Examples/Test Cases**
-- input:
-- Output:
+
+- input: [3, 5, 2]
+- Output: (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+[3], [3, 5], [3,5,2]
+[3, 8, 10]
+21
 
 
 **Questions**
@@ -43,15 +55,24 @@ involved in going from the input to the output
 
 
 **Data Structures**
-- input:
-- Output:
-- Intermediate:
-- Storage:
+- input: array
+- Output: number /integer
+- Intermediate: array of arrays
+- Storage: array
 
 --------------- A:- 12 MIN to 20 MIN------------
 
 **Algorithm**
--
+- Given an array of nums
+- find all the leading subsequences of the array
+  - init an empty array lead_subs = []
+    For end index from 1 to size - 1
+    - save array[0, end_indx] lead_subs
+- find the sum of all the leading subsequences
+
+- find the sum of the sums
+-   make a one dimensional array of lead_subs
+- sum all the elements of lead_subs
 
 
 **Implementation details**
@@ -62,5 +83,25 @@ involved in going from the input to the output
 
 **CodeWithIntent****
 =end
+
+def find_leading_subs(ary)
+  lead_subs = []
+
+  0.upto(ary.size - 1) do |end_idx|
+    lead_subs << ary[0..end_idx]
+  end
+  lead_subs
+end
+
+def sum_of_sums(ary)
+  lead_subs = find_leading_subs(ary)
+
+  lead_subs.flatten.sum
+end
+
+p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+p sum_of_sums([4]) == 4
+p sum_of_sums([1, 2, 3, 4, 5]) == 35
 
 
